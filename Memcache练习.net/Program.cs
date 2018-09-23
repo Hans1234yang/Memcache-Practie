c65d496f6d27fbc02db0b26bd88eaf2f50238cc3
Hans Yang <545872067@qq.com>
2018-09-23 18:46:04 +0800

    mecache 练习

---
+++ Memcache练习.net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Memcache练习.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
---
+++ memcache/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Memcached.ClientLibrary;
+namespace memcache
+{
+    //在程序中使用memcachae 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.架构服务器集群
+            string[] ips = new string[]
+            {
+                "192.168.43.67:11211",
+                 "192.168.43.68:11211"
+            };
+            //通信池的初始化
+            SockIOPool pool = SockIOPool.GetInstance();
+            pool.SetServers(ips);
+            pool.Initialize();
+
+            //创建客户端对象，完成通信
+            MemcachedClient client = new MemcachedClient();
+            client.EnableCompression = false;
+            client.Set("hans","abcdef",600);
+
+            Console.WriteLine("OK");
+            Console.ReadKey();
+
+        }
+    }
+}
